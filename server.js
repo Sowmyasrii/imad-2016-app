bb04f7a464c1c2159f3977a0cfd8d7c825bbfd98
Sowmyasrii <srisowmya211995@gmail.com>
2016-10-04 10:49:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','article-one.html'));
+app.get('/article-one',function(req,res){
+    res.sendFile(path.join(_dirname, 'ui', 'article-one.html'));
 });
 app.get('/module-three',function(req,res){
     res.send('module three is seen here');
