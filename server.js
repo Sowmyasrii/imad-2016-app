813a906711ac22966940d8043263cc0494765614
Sowmyasrii <srisowmya211995@gmail.com>
2016-10-05 11:13:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 
 app.get('/module-three',function(req,res){
     res.send('module three is seen here');
